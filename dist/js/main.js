1f31394a5be2581a96eab1d80ac298eb262a2795
Dnny <mcmillandanny@gmail.com>
2019-12-31 16:18:22 -0500

    added new imgs

--- dist/js/main.js
+++ dist/js/main.js
@@ -10,7 +10,7 @@ $(".nav-item").on("click", function (e) {
 
 var controller = new ScrollMagic.Controller({
   globalSceneOptions: {
-    duration: 300
+    duration: 400
   }
 }); // build scenes
 
@@ -33,5 +33,40 @@ new ScrollMagic.Scene({
   triggerElement: ".spacer4"
 }).setClassToggle("#high4", "active") // add class toggle
 // .addIndicators() // add indicators (requires plugin)
-.addTo(controller);
+.addTo(controller); // LightBox Modal
+
+jQuery(document).ready(function ($) {
+  $('.lightbox').click(function (e) {
+    //prevent default action (hyperlink)
+    e.preventDefault(); //Get clicked link href
+
+    var image_href = $(this).attr("href");
+    /* 	
+    If the lightbox window HTML already exists in document, 
+    change the img src to to match the href of whatever link was clicked
+    
+    If the lightbox window HTML doesn't exists, create it and insert it.
+    (This will only happen the first time around)
+    */
+
+    if ($('#lightbox').length > 0) {
+      // #lightbox exists
+      //place href as img src value
+      $('#content').html('<img src="' + image_href + '" />'); //show lightbox window - you could use .show('fast') for a transition
+
+      $('#lightbox').show('fast');
+    } else {
+      //#lightbox does not exist - create and insert (runs 1st time only)
+      //create HTML markup for lightbox window
+      var lightbox = '<div id="lightbox">' + '<p>Click to close</p>' + '<div id="content">' + //insert clicked link's href into img src
+      '<img src="' + image_href + '" />' + '</div>' + '</div>'; //insert lightbox HTML into page
+
+      $('body').append(lightbox);
+    }
+  });
+  $('#lightbox').click(function () {
+    alert("clicked");
+    $('#lightbox').css('display', 'none');
+  });
+});
 //# sourceMappingURL=main.js.map
